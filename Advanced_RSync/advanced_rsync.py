7aa80dc90043ce6054f74b35d1b29ef0e4acf4b2
George Adrian Untu <79197593+adrian-untu@users.noreply.github.com>
2023-01-07 02:24:51 +0200

    Made progress regarding continuous sync of files and folders

--- Advanced_RSync/advanced_rsync.py
+++ Advanced_RSync/advanced_rsync.py
@@ -1,6 +1,7 @@
 import sys
 import os
 import shutil
+import time
 
 location1 = sys.argv[1]
 location2 = sys.argv[2]
@@ -75,17 +76,67 @@ def syncFiles(rootDir1, rootDir2):
                 continue
             shutil.copy2(fullPath2, fullPath1)
             print("File " + str(fullPath1) + " copied from " + str(fullPath2))
+
+def generate_list(path):
+    list_for_path = []
+    for root, dirs, files in os.walk(path):
+        for name in files:
+            list_for_path.append([os.path.join(root,name), os.path.getmtime(os.path.join(root,name))])
+            list_for_path.append([os.path.join(root,name), os.path.getmtime(os.path.join(root, name))])
+        for dir in dirs:
+            list_for_path.append([os.path.join(root,dir), os.path.getmtime(os.path.join(root, dir))])
+            list_for_path.append([os.path.join(root, dir), os.path.getmtime(os.path.join(root, dir))])
+    return list_for_path
+
+def delete_folders(rootDir1, rootDir2): #this function will delete all deleted folders from rootDir1 and changes will apply to rootDir2
+    for root2, dirs2, files2 in os.walk(rootDir2):
+        for relativePath2 in dirs2:
+            fullPath2 = os.path.join(root2, relativePath2)
+            fullPath1 = fullPath2.replace(rootDir2, rootDir1)
+            if (not os.path.exists(fullPath1)) and os.path.isdir(fullPath1) :
+                continue
+            if os.path.exists(fullPath1) and os.path.isfile(fullPath1) :
+                raise Exception("Cannot perform dir sync." + str(fullPath1) + " should be a dir, not a file!")
+            # Case 3 : dest dir exists but not src dir, so we need to copy it
+            shutil.rmtree(fullPath2)
+            print("Directory " + str(fullPath2) + " deleted from" + str(rootDir2))
+            continue
+
+def continue_sync(path1,path2):
+    list_for_path1 = generate_list(path1)
+    list_for_path2 = generate_list(path2)
+    time.sleep(1)
+    list_for_path1_second = generate_list(path1)
+    list_for_path2_second = generate_list(path2)
+    print(list_for_path1)
+    print(list_for_path1_second)
+    time.sleep(1)
+    print(list_for_path2)
+    print(list_for_path2_second)
+    if len(list_for_path1_second) > len(list_for_path2_second) and len(list_for_path1) == len(list_for_path2):
+        print("list1>list2")
+        delete_folders(path2,path1)
+    else:
+        print("list2>list1")
+        syncDirs(path1,path2)
+        syncFiles(path1,path2)
+        #delete_folders(path1,path2)
+
+def initial_step(path1,path2):
+    try:
+        checkIfRootDirsExist(path1, path2)
+        syncDirs(path1, path2)
+        syncFiles(path1, path2)
+    except Exception as e:
+        print(e)
+        print("End sync with failure!")
 def sync(path1,path2):
+    print("Doing initial sync...")
+    initial_step(path1,path2)
+    print("First sync done, proceeding with sync of files")
     while 1:
-        try:
-            checkIfRootDirsExist(path1, path2)
-            syncDirs(path1, path2)
-            syncFiles(path1, path2)
-        except Exception as e:
-            print(e)
-            print("End sync with failure!")
-
-
+        time.sleep(1)
+        continue_sync(path1,path2)
 
 def rsync(location1, location2):
     type1 = get_type_of_location(location1)
